5f4f46bbe6e5daf43feef724795fabb62cefe299
Devaev Maxim <mdevaev@gmail.com>
2020-07-02 23:02:40 +0300

    more omx error codes

--- src/encoders/omx/formatters.c
+++ src/encoders/omx/formatters.c
@@ -67,15 +67,38 @@ const char *omx_error_to_string(OMX_ERRORTYPE error) {
 		CASE_TO_STRING(OMX_ErrorPortUnresponsiveDuringDeallocation);
 		CASE_TO_STRING(OMX_ErrorPortUnresponsiveDuringStop);
 		CASE_TO_STRING(OMX_ErrorIncorrectStateTransition);
+		CASE_TO_STRING(OMX_ErrorIncorrectStateOperation);
+		CASE_TO_STRING(OMX_ErrorUnsupportedSetting);
+		CASE_TO_STRING(OMX_ErrorUnsupportedIndex);
+		CASE_TO_STRING(OMX_ErrorBadPortIndex);
+		CASE_TO_STRING(OMX_ErrorPortUnpopulated);
+		CASE_TO_STRING(OMX_ErrorComponentSuspended);
+		CASE_TO_STRING(OMX_ErrorDynamicResourcesUnavailable);
+		CASE_TO_STRING(OMX_ErrorMbErrorsInFrame);
+		CASE_TO_STRING(OMX_ErrorFormatNotDetected);
+		CASE_TO_STRING(OMX_ErrorContentPipeOpenFailed);
+		CASE_TO_STRING(OMX_ErrorContentPipeCreationFailed);
+		CASE_TO_STRING(OMX_ErrorSeperateTablesUsed);
+		CASE_TO_STRING(OMX_ErrorTunnelingUnsupported);
+		CASE_TO_STRING(OMX_ErrorKhronosExtensions);
+		CASE_TO_STRING(OMX_ErrorVendorStartUnused);
+		CASE_TO_STRING(OMX_ErrorDiskFull);
+		CASE_TO_STRING(OMX_ErrorMaxFileSize);
+		CASE_TO_STRING(OMX_ErrorDrmUnauthorised);
+		CASE_TO_STRING(OMX_ErrorDrmExpired);
+		CASE_TO_STRING(OMX_ErrorDrmGeneral);
 		default: return "Unknown OMX error";
 	}
 }
 
 const char *omx_state_to_string(OMX_STATETYPE state) {
 	switch (state) {
+		CASE_TO_STRING(OMX_StateInvalid);
 		CASE_TO_STRING(OMX_StateLoaded);
 		CASE_TO_STRING(OMX_StateIdle);
 		CASE_TO_STRING(OMX_StateExecuting);
+		CASE_TO_STRING(OMX_StatePause);
+		CASE_TO_STRING(OMX_StateWaitForResources);
 		// cppcheck-suppress constArgument
 		// cppcheck-suppress knownArgument
 		CASE_ASSERT("Unsupported OMX state", state);
