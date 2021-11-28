421f4a1f2e34850554fe9f83610a60a833a1ff98
Maxim Devaev <mdevaev@gmail.com>
2021-11-28 08:43:41 +0300

    Bump version: 4.9 → 4.10

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="ustreamer",
-        version="4.9",
+        version="4.10",
         description="uStreamer tools",
         author="Maxim Devaev",
         author_email="mdevaev@gmail.com",
