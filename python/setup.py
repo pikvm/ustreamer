5502758a7ebdfbc6c8960ea50b26bf808aec75cc
Maxim Devaev <mdevaev@gmail.com>
2022-07-30 13:10:16 +0300

    Bump version: 5.18 → 5.19

--- python/setup.py
+++ python/setup.py
@@ -19,7 +19,7 @@ def _find_sources(suffix: str) -> List[str]:
 if __name__ == "__main__":
     setup(
         name="ustreamer",
-        version="5.18",
+        version="5.19",
         description="uStreamer tools",
         author="Maxim Devaev",
         author_email="mdevaev@gmail.com",
