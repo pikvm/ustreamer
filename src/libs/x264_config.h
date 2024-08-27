085bfd316d2fec41c593191825b398f762b4a2a1
mofeng-git <mofeng654321@hotmail.com>
2024-08-27 14:03:10 +0000

    补充文件头

---
+++ src/libs/x264_config.h
@@ -0,0 +1,8 @@
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     0
+#define X264_CHROMA_FORMAT 0
+#define X264_REV 3191
+#define X264_REV_DIFF 0
+#define X264_VERSION " r3191 4613ac3"
+#define X264_POINTVER "0.164.3191 4613ac3"
