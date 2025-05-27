0e521ad0c6cdb8869d259f801e71d276373fe4ab
Maxim Devaev <mdevaev@gmail.com>
2025-05-27 19:42:34 +0300

    Bump version: 6.36 → 6.37

--- python/setup.py
+++ python/setup.py
@@ -34,7 +34,7 @@ def main() -> None:
     flags = _find_flags()
     setup(
         name="ustreamer",
-        version="6.36",
+        version="6.37",
         description="uStreamer tools",
         author="Maxim Devaev",
         author_email="mdevaev@gmail.com",
