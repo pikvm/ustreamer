pikvm/ustreamer

commit 2dac4db9fc5995462f96ad89e35fb219be575643

janus: fixed first/last frame logic for sps/pps

janus/src/rtpv.c
@@ -65,8 +65,6 @@ void us_rtpv_wrap(us_rtpv_s *rtpv, const us_frame_s *frame, bool zero_playout_de
 
 	assert(frame->format == V4L2_PIX_FMT_H264);
 
-	rtpv->rtp->first_of_frame = true;
-	rtpv->rtp->last_of_frame = false;
 	rtpv->rtp->zero_playout_delay = zero_playout_delay;
 	rtpv->rtp->grab_ntp_ts = us_get_now_ntp() - us_ld_to_ntp(us_get_now_monotonic() - frame->grab_begin_ts);
 
@@ -105,30 +103,18 @@ void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool 
 	const uint type = data[0] & 0x1F;
 	u8 *dg = rtpv->rtp->datagram;
 
-	// Set *_of_frame flags only for non-SPS/PPS packages
-/*
-#	define CALL_FOR_SERVICE { \
-			const bool m_fof = rtpv->rtp->first_of_frame; \
-			const bool m_lof = rtpv->rtp->last_of_frame; \
-			rtpv->rtp->first_of_frame = false; \
-			rtpv->rtp->last_of_frame = false; \
-			rtpv->callback(rtpv->rtp); \
-			rtpv->rtp->first_of_frame = m_fof; \
-			rtpv->rtp->last_of_frame = m_lof; \
-		}
-*/
-
 	if (size + US_RTP_HEADER_SIZE <= US_RTP_TOTAL_SIZE) {
 		us_rtp_write_header(rtpv->rtp, pts, marked);
 		memcpy(dg + US_RTP_HEADER_SIZE, data, size);
 		rtpv->rtp->used = size + US_RTP_HEADER_SIZE;
-//		if (type == 7 || type == 8) {
-//			CALL_FOR_SERVICE;
-//		} else {*/
-			rtpv->rtp->last_of_frame = true;
-			rtpv->callback(rtpv->rtp);
+		if (type == 7 || type == 8) { // SPS || PPS
 			rtpv->rtp->first_of_frame = false;
-//		}
+			rtpv->rtp->last_of_frame = false;
+		} else {
+			rtpv->rtp->first_of_frame = true;
+			rtpv->rtp->last_of_frame = true;
+		}
+		rtpv->callback(rtpv->rtp);
 		return;
 	}
 
@@ -160,20 +146,14 @@ void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool 
 
 		memcpy(dg + fu_overhead, src, frag_size);
 		rtpv->rtp->used = fu_overhead + frag_size;
-//		if (type == 7 || type == 8) {
-//			CALL_FOR_SERVICE;
-//		} else {
-			rtpv->rtp->last_of_frame = last;
-			rtpv->callback(rtpv->rtp);
-			rtpv->rtp->first_of_frame = false;
-//		}
+		rtpv->rtp->first_of_frame = first;
+		rtpv->rtp->last_of_frame = last;
+		rtpv->callback(rtpv->rtp);
 
 		src += frag_size;
 		remaining -= frag_size;
 		first = false;
 	}
-
-#	undef CALL_FOR_SERVICE
 }
 
 static sz _find_annexb(const u8 *data, uz size) {

commit 0fbbc27d8c16d5949eaf16299138477e26990c16

janus: rtpv: fix and rework {first,last}_of_frame determination
Fixes: 2dac4db ("janus: fixed first/last frame logic for sps/pps")
Fixes: f32e256 ("janus: refactoring")

janus/src/rtpv.c
@@ -36,7 +36,7 @@
 #include "rtp.h"
 
 
-void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool last_nalu);
+void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool first_nalu, bool last_nalu);
 
 static sz _find_annexb(const u8 *data, uz size);
 
@@ -80,6 +80,7 @@ void us_rtpv_wrap(us_rtpv_s *rtpv, const us_frame_s *frame, bool zero_playout_de
 	}
 	uz begin = offset_to_first + _PRE;
 
+	bool first_nalu = true;
 	while (begin < frame->used) { // Find and iterate by NALUs
 		const u8 *const data = frame->data + begin;
 		const sz offset_to_next = _find_annexb(data, frame->used - begin);
@@ -90,21 +91,23 @@ void us_rtpv_wrap(us_rtpv_s *rtpv, const us_frame_s *frame, bool zero_playout_de
 				--size;
 			}
 			if (size > 1) { // Skip too short NALUs
-				_rtpv_process_nalu(rtpv, data, size, pts, false);
+				_rtpv_process_nalu(rtpv, data, size, pts, first_nalu, false);
+				first_nalu = false;
 			}
 			begin += offset_to_next + _PRE;
 
 		} else { // Process the tail
 			const uz size = frame->used - begin;
 			if (size > 1) {
-				_rtpv_process_nalu(rtpv, data, size, pts, true);
+				_rtpv_process_nalu(rtpv, data, size, pts, first_nalu, true);
+				first_nalu = false;
 			}
 			break;
 		}
 	}
 }
 
-void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool last_nalu) {
+void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool first_nalu, bool last_nalu) {
 	US_A(size > 1);
 
 	const uint ref_idc = (data[0] >> 5) & 3;
@@ -116,9 +119,8 @@ void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool 
 		us_rtp_write_header(rtp, pts, last_nalu);
 		memcpy(dg + US_RTP_HEADER_SIZE, data, size);
 		rtp->used = size + US_RTP_HEADER_SIZE;
-		const bool sps_or_pps = (type == 7 || type == 8);
-		rtp->first_of_frame = !sps_or_pps;
-		rtp->last_of_frame = !sps_or_pps;
+		rtp->first_of_frame = first_nalu;
+		rtp->last_of_frame = last_nalu;
 		rtpv->callback(rtp);
 		return;
 	}
@@ -151,8 +153,8 @@ void _rtpv_process_nalu(us_rtpv_s *rtpv, const u8 *data, uz size, u32 pts, bool 
 
 		memcpy(dg + fu_overhead, src, frag_size);
 		rtp->used = fu_overhead + frag_size;
-		rtp->first_of_frame = first;
-		rtp->last_of_frame = last;
+		rtp->first_of_frame = first_nalu && first;
+		rtp->last_of_frame = last_nalu && last;
 		rtpv->callback(rtp);
 
 		src += frag_size;